bf3e9bbecf7250a2037081489a84816c19d145ed
Adrian Becares <abecares001@gmail.com>
2021-07-03 20:38:15 +0200

    Ready for long test 03.07.2021

--- main/main.py
+++ main/main.py
@@ -6,13 +6,11 @@ import mne
 import keras.utils
 import pandas as pd
 import ray
-from keras.callbacks import Callback
-import matplotlib.pyplot as plt
 from tensorflow.keras.utils import Sequence
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.callbacks import CSVLogger
 from sklearn.model_selection import KFold
-from scikitplot.metrics import plot_confusion_matrix, plot_roc
+from sklearn.metrics import confusion_matrix
 from ray import tune
 from ray.tune.schedulers import AsyncHyperBandScheduler
 from ray.tune.suggest.hyperopt import HyperOptSearch
@@ -170,7 +168,7 @@ class BlackBox(tune.Trainable):
         loss_per_fold = []
         folds_histories = {}
         
-        kfold = KFold(n_splits=2, shuffle=True) # 10-Fold Cross-Validation
+        kfold = KFold(n_splits=10, shuffle=True) # 10-Fold Cross-Validation
         
         fold = 1
         for train, test in kfold.split(inputs, targets):
@@ -194,13 +192,17 @@ class BlackBox(tune.Trainable):
             history = model.fit(inputs[train], 
                         targets[train],
                         batch_size=32,
-                        epochs=1,
+                        epochs=100,
                         callbacks=[early_stopping, csv_logger],
                         validation_split=0.2,
                         verbose=2)
             
             scores = model.evaluate(inputs[test], targets[test], verbose=0)
             #print(scores)
+            predictions = model.predict(inputs[test])
+            y_pred = (predictions > 0.5)
+            matrix = confusion_matrix(targets[test].argmax(axis=1), y_pred.argmax(axis=1))
+            np.savetxt(f'confusion_matrix_fold_{fold}.csv', matrix, delimiter=',')
             fold_val_loss = scores[0]
             fold_val_accuracy = scores[1]
             print('--------------------------------------------------------------------------------------------------')
@@ -237,7 +239,7 @@ if __name__ == "__main__":
               metric='val_accuracy',
               stop={'training_iteration': 1},
               scheduler=scheduler,
-              num_samples=1,
+              num_samples=250,
               config={
                   "window_size": tune.quniform(50,100,50),
                   "step_size": tune.quniform(0.1,1,0.1),
@@ -296,32 +298,7 @@ if __name__ == "__main__":
                     n_conv_layers=n_conv_layers,
                     n_fc_layers=n_fc_layers,
                     n_neurons_2nd_layer=n_neurons_2nd_layer,
-                    dropout_rate=dropout_rate)    
-    
-    class PerformanceVisualizationCallback(Callback):
-        def __init__(self, model, validation_data:
-            super().__init__()
-            self.model = model
-            self.validation_data = validation_data
-
-        def on_epoch_end(self, epoch, logs={}):
-            y_pred = np.asarray(self.model.predict(self.validation_data[0]))
-            y_true = self.validation_data[1]             
-            y_pred_class = np.argmax(y_pred, axis=1)
-
-            # plot and save confusion matrix
-            fig, ax = plt.subplots(figsize=(16,12))
-            plot_confusion_matrix(y_true, y_pred_class, ax=ax)
-            fig.savefig(fr'/home/keras/ray_results/{name}/confusion_matrix_epoch_{epoch}')
-
-            # plot and save roc curve
-            fig, ax = plt.subplots(figsize=(16,12))
-            plot_roc(y_true, y_pred, ax=ax)
-            fig.savefig(fr'/home/keras/ray_results/{name}/roc_curve_epoch_{epoch}')
-    
-    performance_cbk = PerformanceVisualizationCallback(
-                      model=model,
-                      validation_data=data['y_test'])
+                    dropout_rate=dropout_rate)
 
     early_stopping = EarlyStopping(monitor='accuracy', min_delta=1e-3, mode='max', patience=5)
     csv_logger = CSVLogger(fr'/home/keras/ray_results/{name}/final_model_history.csv')
@@ -330,15 +307,20 @@ if __name__ == "__main__":
                          data['y_train'],
                          batch_size=32,
                          epochs=100,
-                         callbacks=[early_stopping, csv_logger, performance_cbk],
                          validation_split=0.2,
+                         callbacks=[early_stopping, csv_logger],
                          verbose=2)
      
     scores = model.evaluate(data['x_test'], data['y_test'], verbose=1)
     #model.save(fr'/home/keras/ray_results/{name}/final_model_{name}')
-     
+    predictions = model.predict(data['x_test'])
+    y_pred = (predictions > 0.5)
+    matrix = confusion_matrix(data['y_test'].argmax(axis=1), y_pred.argmax(axis=1))
+    np.savetxt(fr'/home/keras/ray_results/{name}/confusion_matrix.csv', matrix, delimiter=',')    
+ 
     val_loss = scores[0]
     val_accuracy = scores[1]
           
     print(f'Final score: val_loss of {val_loss}; val_accuracy of {val_accuracy}')
+    print('Confusion matrix:', matrix)
 
